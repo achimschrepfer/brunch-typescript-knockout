7eaaf0625e7a906f0ca1edce6957d50aedaf4703
Achim Schrepfer <aschrepfer@Achims-MBP.fritz.box>
2017-03-03 15:45:41 +0100

    Working ASP.NET MVC App with... ...all the stuff we want to run: Brunch, TypeScript, Knockout w/ external components, jQuery

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace brunchapp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Simple Brunch TypeScript Demo";
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
